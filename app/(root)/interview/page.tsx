b8f6ae1f7b727f2e8e27880c0fbd6735a9178fa4
Prateek Rana <prateekrana758@gmail.com>
2025-05-16 17:46:32 +0530

    implement questions generation

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,17 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+    <h3>Interview Generation</h3>
+    <Agent 
+    userName='YOu'
+    userId='user1'
+    type='generate'
+    />
+    </>
+  )
+}
+
+export default Page
